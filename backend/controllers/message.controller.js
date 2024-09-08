5405974409250630e6668d9a0700cfd930bed2e2
Emgmbakri <mrm507415@gmail.com>
2024-09-09 01:28:47 +0400

    chore(server): Update server to include user routes and middleware. Added dotenv configuration to load environment variables. Included  middleware to handle cookies for authentication. Registered the new user routes () alongside authentication and message routes.

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -25,17 +25,44 @@ export const sendMessage = async (req, res) => {
 			message,
 		});
 
-		await newMessage.save();
-
 		if (newMessage) {
 			conversation.message.push(newMessage._id);
-			await conversation.save();
 		}
+
+		// Socket Io Functionality Will Go Here
+
+		//await conversation.save();
+		//await newMessage.save();
+		
+		// This Will Run In Parallel
+		await Promise.all([conversation.save(), newMessage.save()]);
 
 		res.status(201).json(newMessage);
 
 	} catch (error) {
 		console.log("Error in sendMessage Controller: ", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
+
+export const getMessage = async (req, res) => {
+	try {
+		const {id:userToChatId} = req.params;
+		const senderId = req.user._id;
+
+		const conversation = await Conversation.findOne({
+			participants: { $all: [senderId, userToChatId] },
+
+		}).populate("message");
+
+		if(!conversation) return res.status(201).json([]);
+		
+		const messages = conversation.message;
+
+		res.status(200).json(messages); // not reference but the actual message
+
+	} catch (error) {
+		console.log("Error in getMessage Controller: ", error.message);
 		res.status(500).json({ error: "Internal Server Error" });
 	}
 };
