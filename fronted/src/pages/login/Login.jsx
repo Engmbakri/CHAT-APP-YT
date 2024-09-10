4c201cfdfd189b3dce31e857f11e93bd1039a625
Emgmbakri <mrm507415@gmail.com>
2024-09-10 20:58:26 +0400

    Added username and  password fields to Login component: - Included username and password input fields with a label for user password and username entry

--- fronted/src/pages/login/Login.jsx
+++ fronted/src/pages/login/Login.jsx
@@ -4,8 +4,23 @@ const Login = () => {
             <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
                 <h1 className='text-3xl font-semibold text-center text-gray-300'>
                     Login
-	    	    <span className='text-blue-500'> ChatApp</span>
+                    <span className='text-blue-500'> ChatApp</span>
                 </h1>
+
+                <form>
+                    <div>
+                        <label className='label p-2'>
+                            <span className='text-base label-text'>Username</span>
+                        </label>
+                        <input type='text' placeholder='Enter username' className='w-full input input-bordered h-10'/>
+                    </div>
+	            <div>
+                        <label className='label p-2'>
+                            <span className='text-base label-text'>Password</span>
+                        </label>
+                        <input type='password' placeholder='Enter Password' className='w-full input input-bordered h-10'/>
+                    </div>
+                </form>
             </div>
         </div>
     );
