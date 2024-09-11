1255185dd2835e2908099cfdfc7f695e901a0a12
Emgmbakri <mrm507415@gmail.com>
2024-09-11 19:36:57 +0400

    added sidebar component

--- fronted/src/pages/signup/GenderCheckbox.jsx
+++ fronted/src/pages/signup/GenderCheckbox.jsx
@@ -23,3 +23,31 @@ const GenderCheckbox = () => {
 	);
 };
 export default GenderCheckbox;
+
+
+// STARTER CODE FOR THIS FILE
+// const GenderCheckbox = () => {
+        // return (
+                // <div className='flex'>
+                        // <div className='form-control'>
+                                // <label className={`label gap-2 cursor-pointer`}>
+                                        // <span className='text-base text-black'>Male</span>
+                                        // <input
+                                                // type='checkbox'
+                                                // className='checkbox border-slate-900'
+                                        // />
+                                // </label>
+                        // </div>
+                        // <div className='form-control'>
+                                // <label className={`label gap-2 cursor-pointer`}>
+                                        // <span className='text-base text-black'>Female</span>
+                                        // <input
+                                                // type='checkbox'
+                                                // className='checkbox border-slate-900'
+                                        // />
+                                // </label>
+                        // </div>
+                // </div>
+        // );
+// };
+// export default GenderCheckbox;
