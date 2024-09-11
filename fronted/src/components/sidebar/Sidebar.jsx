9d4a8dba2ce7bbe6ddf12d26a91c90712e621785
Emgmbakri <mrm507415@gmail.com>
2024-09-12 01:24:38 +0400

    Updated Sidebar component to include LogoutButton for user logout functionality

--- fronted/src/components/sidebar/Sidebar.jsx
+++ fronted/src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
 import SearchInput from "./SearchInput";
 import Conversations from "./Conversations";
+import LogoutButton from "./LogoutButton";
 
 const Sidebar = () => {
 	return (
@@ -7,10 +8,25 @@ const Sidebar = () => {
             <SearchInput />
 	    <div className='divider px-3'></div>
 	    <Conversations />
-
-	    {/*<Conversations />
-	    <LogoutButton />*/}
+	    <LogoutButton />
 	  </div>
 	);
 };
 export default Sidebar;
+
+// STARTER CODE FOR THIS FILE
+// import Conversations from "./Conversations";
+// import LogoutButton from "./LogoutButton";
+// import SearchInput from "./SearchInput";
+
+// const Sidebar = () => {
+// 	return (
+// 		<div className='border-r border-slate-500 p-4 flex flex-col'>
+// 			<SearchInput />
+// 			<div className='divider px-3'></div>
+// 			<Conversations />
+// 			<LogoutButton />
+// 		</div>
+// 	);
+// };
+// export default Sidebar;
