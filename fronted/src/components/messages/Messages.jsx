f4e66c4af90cc99dc72912363397803730e85691
Emgmbakri <mrm507415@gmail.com>
2024-09-14 14:20:43 +0400

    Configure Vite proxy for API requests to backend server

--- fronted/src/components/messages/Messages.jsx
+++ fronted/src/components/messages/Messages.jsx
@@ -19,4 +19,28 @@ const Messages = () => {
 	);
 };
 
-gxport default Messages;
+export default Messages;
+
+
+// STARTER CODE SNIPPET
+// import Message from "./Message";
+//
+// const Messages = () => {
+// return (
+//   <div className='px-4 flex-1 overflow-auto'>
+//     <Message />
+//     <Message />
+//     <Message />
+//     <Message />
+//     <Message />
+//     <Message />
+//     <Message />
+//     <Message />
+//     <Message />
+//     <Message />
+//     <Message />
+//     <Message />
+//   </div>
+//   );
+// };
+
