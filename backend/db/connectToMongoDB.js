9265e156b64cfa87ead364b31d6dfcf4079e2a2f
Emgmbakri <mrm507415@gmail.com>
2024-09-18 18:32:40 +0400

    Database connection functionality

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO_DB_URL);
-		console.log("connected to momgo");
+		await mongoose.connect(process.env.MONGO_DB_URI);
+		console.log("Connected to MongoDB");
 	} catch (error) {
-		console.log("error while connecting to mongo", error.message);
+		console.log("Error connecting to MongoDB", error.message);
 	}
 };
 
