9430e47094f60fd04a37dba25d05eb8d14b79be7
Emgmbakri <mrm507415@gmail.com>
2024-09-18 18:24:40 +0400

    Added email field to user schema with validation and unique constraint. Added fields for password reset functionality (resetPasswordToken, resetPasswordExpires).

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,32 +1,41 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-	fullName: {
-		type: String,
-		required: true,
+const userSchema = new mongoose.Schema(
+	{
+		fullName: {
+			type: String,
+			required: true,
+		},
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+			minlength: 6,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true, // Ensure emails are unique
+			match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'], // Simple email validation
+		},
+		gender: {
+			type: String,
+			required: true,
+			enum: ["male", "female"],
+		},
+		profilePic: {
+			type: String,
+			default: "",
+		},
+		resetPasswordToken: { type: String }, // Token for resetting password
+		resetPasswordExpires: { type: Date }, // Token expiration time
+		// createdAt, updatedAt => Member since <createdAt>
 	},
-	username: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	password: {
-		type: String,
-		required: true,
-		minlength: 6,
-	},
-	gender: {
-		type: String,
-		required: true,
-		enum: ["male", "female"]
-	},
-	profilePic: {
-		type: String,
-		default: "",
-	},
-	// craetedAt, updatedAt => Member since <createdAt>
-},
-	{timestamps: true}
+	{ timestamps: true }
 );
 
 const User = mongoose.model("User", userSchema);
